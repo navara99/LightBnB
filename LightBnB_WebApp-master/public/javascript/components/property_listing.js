ae13ecc8b0cc71804c57de55262a541cb16f2b39
Tharsikan <thar.navaratnam@gmail.com>
2021-11-25 22:53:11 +0000

    add making reservations feature (stretch)

--- LightBnB_WebApp-master/public/javascript/components/property_listing.js
+++ LightBnB_WebApp-master/public/javascript/components/property_listing.js
@@ -1,6 +1,6 @@
 $(() => {
   window.propertyListing = {};
-  
+
   function createListing(property, isReservation) {
     return `
     <article class="property-listing">
@@ -14,12 +14,19 @@ $(() => {
             <li>number_of_bathrooms: ${property.number_of_bathrooms}</li>
             <li>parking_spaces: ${property.parking_spaces}</li>
           </ul>
-          ${isReservation ? 
-            `<p>${moment(property.start_date).format('ll')} - ${moment(property.end_date).format('ll')}</p>` 
-            : ``}
+          ${isReservation ?
+        `<p>${moment(property.start_date).format('ll')} - ${moment(property.end_date).format('ll')}</p>`
+        : ``}
           <footer class="property-listing__footer">
             <div class="property-listing__rating">${Math.round(property.average_rating * 100) / 100}/5 stars</div>
-            <div class="property-listing__price">$${property.cost_per_night/100.0}/night</div>
+            <div class="property-listing__price">$${property.cost_per_night / 100.0}/night</div>
+            <form type="submit" action="api/reservations/${property.id}" method="POST">
+            <label for="start_date" >Start Date</label>
+            <input name="start_date" type="date"/>
+            <label for="end_date">End Date</label>
+            <input name="end_date" type="date"/>
+            <button type="submit">Make Reservation</button>
+            </form>
           </footer>
         </section>
       </article>
--- LightBnB_WebApp-master/server/apiRoutes.js
+++ LightBnB_WebApp-master/server/apiRoutes.js
@@ -1,12 +1,12 @@
-module.exports = function(router, database) {
+module.exports = function (router, database) {
 
   router.get('/properties', (req, res) => {
     database.getAllProperties(req.query, 20)
-    .then(properties => res.send({properties}))
-    .catch(e => {
-      console.error(e);
-      res.send(e)
-    }); 
+      .then(properties => res.send({ properties }))
+      .catch(e => {
+        console.error(e);
+        res.send(e)
+      });
   });
 
   router.get('/reservations', (req, res) => {
@@ -16,22 +16,46 @@ module.exports = function(router, database) {
       return;
     }
     database.getAllReservations(userId)
-    .then(reservations => res.send({reservations}))
-    .catch(e => {
-      console.error(e);
-      res.send(e)
-    });
+      .then(reservations => res.send({ reservations }))
+      .catch(e => {
+        console.error(e);
+        res.send(e)
+      });
   });
 
   router.post('/properties', (req, res) => {
     const userId = req.session.userId;
-    database.addProperty({...req.body, owner_id: userId})
+    database.addProperty({ ...req.body, owner_id: userId })
       .then(property => {
         res.send(property);
       })
       .catch(e => {
         console.error(e);
         res.send(e)
+      });
+  });
+
+  router.post('/reservations/:property_id', (req, res) => {
+    const { property_id } = req.params;
+    const guest_id = req.session.userId;
+    const reservationDetails = {
+      ...req.body,
+      property_id,
+      guest_id,
+    };
+
+    database.makeReservation(reservationDetails)
+      .then((reservation) => {
+        const { start_date, end_date } = reservation[0];
+        res.send(`
+      You have successfuly booked a reservation. 
+      Your start date is ${start_date} and your end date is ${end_date}.
+      Thank you for using LightBnB. 
+      `);
+      })
+      .catch((e) => {
+        console.error(e);
+        res.send(e);
       });
   });
 
